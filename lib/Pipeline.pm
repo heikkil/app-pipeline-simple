6e051f09bb3f07af47f20d78080b37b9938bdca9
Heikki Lehvaslaiho <Heikki.Lehvaslaiho@kaust.edu.sa>
2010-04-22 12:08:56 +0300

    config parsing works

--- lib/Pipeline.pm
+++ lib/Pipeline.pm
@@ -1,168 +1,242 @@
+#-----------------------------------------------------------------
+# Pipeline
+# Authors:Heikki Lehvasllaiho <heikki.lehvaslaiho@gmail.com>
+# For copyright and disclaimer see Pipeline pod.
+#
+# Lghtweight workflow manager
+
 package Pipeline;
 
-use warnings;
 use strict;
+use warnings;
+use vars qw( $AUTOLOAD );
+
 use Carp;
+use File::Basename;
+use XML::Simple;
+use Data::Dumper;
+=pod
 
-use version; $VERSION = qv('0.0.3');
+ sceleton
 
-# Other recommended modules (uncomment to use):
-#  use IO::Prompt;
-#  use Perl6::Export;
-#  use Perl6::Slurp;
-#  use Perl6::Say;
+=cut
+
+#-----------------------------------------------------------------
+# Global variables (available for all packages in this file)
+#-----------------------------------------------------------------
+our $VERSION = '0.1';
+
+#-----------------------------------------------------------------
+# A list of allowed options/arguments (used in the new() method)
+#-----------------------------------------------------------------
+{
+    my %_allowed =
+	(
+
+         id             => 1,
+         name           => 1,
+         description    => 1,
+
+	 input_name     => 1,
+	 input_format   => 1,
+
+	 code           => 1,
+	 next_id        => 1,
+
+	 config         => 1,
+	 add            => 1,
+
+	 dir            => 1,
+	 run            => 1,
+	 stringify       => 1,
+	 );
+
+    sub _accessible {
+	my ($self, $attr) = @_;
+	exists $_allowed{$attr};
+    }
+}
+
+#-----------------------------------------------------------------
+# Deal with 'set' and 'get' methods.
+#-----------------------------------------------------------------
+sub AUTOLOAD {
+    my ($self, $value) = @_;
+    my $ref_sub;
+    if ($AUTOLOAD =~ /.*::(\w+)/ && $self->_accessible ("$1")) {
+
+	# get/set method
+	my $attr_name = "$1";
+	$ref_sub =
+	    sub {
+		# get method
+		local *__ANON__ = "__ANON__$attr_name" . "_" . ref ($self);
+		my ($this, $value) = @_;
+		return $this->{$attr_name} unless defined $value;
+
+		# set method
+		$this->{$attr_name} = $value;
+		return $this->{$attr_name};
+	    };
+
+    } else {
+	throw ("No such method: $AUTOLOAD");
+    }
+
+    no strict 'refs'; 
+    *{$AUTOLOAD} = $ref_sub;
+    use strict 'refs'; 
+    return $ref_sub->($self, $value);
+}
+
+#-----------------------------------------------------------------
+# Keep it here! The reason is the existence of AUTOLOAD...
+#-----------------------------------------------------------------
+sub DESTROY {
+}
+
+#-----------------------------------------------------------------
+# new
+#-----------------------------------------------------------------
+sub new {
+    my ($class, @args) = @_;
+
+    # create an object
+    my $self = bless {}, ref ($class) || $class;
+
+    # set default values
+    $self->{compiled_operations} = {};
+
+    # set all @args into this object with 'set' values
+    my (%args) = (@args == 1 ? (value => $args[0]) : @args);
+    foreach my $key (keys %args) {
+        no strict 'refs';
+        $self->$key ($args {$key});
+    }
+
+    $self->host ($ENV{'MRS_HOST'}) if $ENV{'MRS_HOST'};
+
+    # done
+    return $self;
+}
+
+#-----------------------------------------------------------------
+#
+#-----------------------------------------------------------------
+sub config {
+    my ($self, $config) = @_;
+    if ($config) {
+	$self->{config} = XMLin($config, KeyAttr => {tool => 'id'});
+    }
+    return  $self->{config};
+}
+
+#-----------------------------------------------------------------
+#
+#-----------------------------------------------------------------
+sub dir {
+    my ($self, $dir) = @_;
+    if ($dir) {
+	mkdir $dir unless -e $dir and -d $dir;
+	croak "Can not create project directory $dir"
+            unless -e $dir and -d $dir;
+	$self->{dir} = $dir;
+    }
+    $self->{dir};
+}
+
+#-----------------------------------------------------------------
+#
+#-----------------------------------------------------------------
+sub run {
+    my ($self) = @_;
+
+    croak "Need an output directory" unless $self->{dir};
+
+    #map {print "$_\n"} sort keys %{$self->{config}->{tool}};
+
+    for my $id (sort keys %{$self->{config}->{tool}}) {
+	print "$id\t";
+	#    say "  ", Dumper $self->{config}->{tool}->{$id};
+	print $self->config->{tool}->{$id}->{name};
+	foreach my $arg (@{$self->{config}->{tool}->{$id}->{arg}}) {
+	    #print Dumper $arg;
+	    print " ", $arg->{key}, "=", ($arg->{value} || ''); 
+	}
+#	print "\n\t--> ",  $self->{config}->{tool}->{$id}->{next}->{id} || '', "\n" if defined $self->{config}->{tool}->{$id}->{next};
+	print "\n";
+    }
+}
 
 
-# Module implementation here
+#-----------------------------------------------------------------
+# Render a tool into a command line string
+#-----------------------------------------------------------------
+
+sub render {
+    my ($self, $tool) = @_;
+
+#    print "\n"; print Dumper $tool; print "\n";
+
+    my $str;
+    $str .=  $tool->{name};
+    my $endstr;
+    foreach my $arg (@{$tool->{arg}}) {
+
+	if (defined $arg->{type} and $arg->{type} eq 'str') {
+	    $str .= " ". '$inputstring';
+	    next;
+	}
+
+	if (defined $arg->{type} and $arg->{type} eq 'redir') {
+	    if ($arg->{key} eq 'in') {
+		$endstr .= " < ". $arg->{value}; 
+	    }
+	    elsif ($arg->{key} eq 'out') {
+		$endstr .= " > ". $arg->{value}; 
+	    } else {
+		croak "Unknown key ". $arg->{key};
+	    }
+	    next;
+	}
+
+	if (defined $arg->{value}) {
+	    $str .= " ". $arg->{key}. "=". $arg->{value}; 
+	} else {
+	    $str .= " ". $arg->{key};
+	}
+
+    }
+
+    return $str. $endstr;
+}
+
+sub stringify {
+    my ($self) = shift;
+    print "-" x 50, "\n";
+    for my $id (sort keys %{$self->{config}->{tool}}) {
+	my $toolstring = $self->render($self->config->{tool}->{$id});
+	print "$id\t$toolstring\n";
 
 
-1; # Magic true value required at end of module
+	if (defined $self->{config}->{tool}->{$id}->{next}) {
+	    if (ref($self->{config}->{tool}->{$id}->{next}) eq 'ARRAY') {
+		for my $next (@{$self->{config}->{tool}->{$id}->{next}}) {
+		    print "  --> ", $next->{id}, "\n";
+		}
+	    } else {
+		print "  --> ", $self->{config}->{tool}->{$id}->{next}->{id}, "\n";
+	    }
+	}
+	print "-" x 50, "\n";
+    }
+}
+
+
+1;
 __END__
 
-=head1 NAME
 
-Pipeline - [One line description of module's purpose here]
-
-
-=head1 VERSION
-
-This document describes Pipeline version 0.0.1
-
-
-=head1 SYNOPSIS
-
-    use Pipeline;
-
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-  
-  
-=head1 DESCRIPTION
-
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
-
-
-=head1 INTERFACE 
-
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
-
-
-=head1 DIAGNOSTICS
-
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-Pipeline requires no configuration files or environment variables.
-
-
-=head1 DEPENDENCIES
-
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
-
-=head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
-None reported.
-
-
-=head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
-
-No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<bug-pipeline@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
-
-
-=head1 AUTHOR
-
-Heikki Lehvaslaiho  C<< <heikki.lehvaslaiho@gmail.com> >>
-
-
-=head1 LICENCE AND COPYRIGHT
-
-Copyright (c) 2010, Heikki Lehvaslaiho C<< <heikki.lehvaslaiho@gmail.com> >>. All rights reserved.
-
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
-
-
-=head1 DISCLAIMER OF WARRANTY
-
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
+#-----------------------------------------------------------------
+# only for debugging
+#-----------------------------------------------------------------
--- t/00.load.t
+++ t/00.load.t
@@ -1,7 +1,71 @@
-use Test::More tests => 1;
-
+use Test::More tests => 14;
+use Data::Dumper;
+use File::Spec;
 BEGIN {
-use_ok( 'Pipeline' );
+      use_ok( 'Pipeline' );
+}
+
+sub test_input_file {
+    return File::Spec->catfile('t', 'data', @_);
 }
 
 diag( "Testing Pipeline $Pipeline::VERSION" );
+
+my $p = Pipeline->new;
+ok ref($p) eq 'Pipeline', 'new()';
+
+my $s2 = Pipeline->new(id=> 'S2');
+ok ref($s2) eq 'Pipeline', 'new()';
+
+# method testing
+can_ok $p->add($s2), 'add';
+ok $s2->id() eq 'S2', 'id()';
+ok $s2->name('test'), 'name()';
+ok $s2->name() eq 'test', 'name()';
+ok $s2->description('test'), 'description()';
+ok $s2->next_id('test'), 'next_id()';
+ok $s2->config(test_input_file('string_manipulation.xml')), 'config()';
+ok $s2->input_name('test'), 'input_name()';
+ok $s2->input_format('test'), 'input_format()';
+ok $s2->code('test'), 'code()';
+#ok $s2->run('test'), 'run()';
+
+#ok $s2->dir('data'), 'dir()';
+#$s2->('test'), '()';
+
+my @methods = qw(id name description input_name   
+		 input_format code next_id config add
+		 run
+	       );
+can_ok 'Pipeline', @methods;
+
+# reading in a configuration
+my $pl = Pipeline->new(config=>test_input_file('string_manipulation.xml'));
+$pl->dir('/tmp/pl');
+$pl->stringify;
+
+#print Dumper $pl;
+
+
+
+
+# # Various ways to say "ok"
+#     ok($got eq $expected, $test_name);
+#
+#     is  ($got, $expected, $test_name);
+#     isnt($got, $expected, $test_name);
+#
+#     # Rather than print STDERR "# here's what went wrong\n"
+#     diag("here's what went wrong");
+#
+#     like  ($got, qr/expected/, $test_name);
+#     unlike($got, qr/expected/, $test_name);
+#
+#     cmp_ok($got, '==', $expected, $test_name);
+#
+#      can_ok($module, @methods);
+#     isa_ok($object, $class);
+#
+#     pass($test_name);
+#     fail($test_name);
+  
